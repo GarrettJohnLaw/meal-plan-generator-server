7f26dd9e597a5e20a18ae9c7d04e4aad3e1cc933
Garrett John Law <law.garrettjohn@gmail.com>
2021-12-02 20:35:06 -0500

    Create MealPlanController.java

---
+++ src/main/java/com/techelevator/controller/MealPlanController.java
@@ -0,0 +1,13 @@
+package com.techelevator.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class MealPlanController {
+
+
+
+
+}
